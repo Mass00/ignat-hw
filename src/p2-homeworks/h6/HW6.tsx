2dbb5ed92c22ba0e31e7b17ed73b0bf4dc100f12
Mass00 <krivmann00@gmail.com>
2022-08-08 16:09:23 +0200

    hw6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
-
+import st from './HW6.module.css'
 function HW6() {
     const [value, setValue] = useState<string>('')
 
@@ -10,29 +10,35 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState('editable-span-value', ''))
     }
 
     return (
+
         <div>
             <hr/>
             homeworks 6
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                {/*should work (должно работать)*/}
+                <div>
+                        <SuperEditableSpan
+                            value={value}
+                            onChangeText={setValue}
+                            spanProps={{children: value ? undefined : 'enter text...'}}
+                        />
+                    <div className={st.btngrp}>
+                        <SuperButton className={st.btn} onClick={save}>save</SuperButton>
+                        <SuperButton className={st.btn} onClick={restore}>restore</SuperButton>
+                    </div>
+                </div>
+                <div>
+                    {/*для личного творчества, могу проверить*/}
+                    {/*<AlternativeSuperEditableSpan/>*/}
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
+                    <hr/>
+                    <hr/>
+                </div>
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
         </div>
     )
 }
--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,6 +1,6 @@
 import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from 'react'
 import SuperInputText from '../../../h4/common/c1-SuperInputText/SuperInputText'
-
+import st from './SuperEditableSpan.module.css'
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 // тип пропсов обычного спана
@@ -31,22 +31,21 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     const {children, onDoubleClick, className, ...restSpanProps} = spanProps || {}
 
     const onEnterCallback = () => {
-        // setEditMode() // выключить editMode при нажатии Enter
-
-        onEnter && onEnter()
+        setEditMode(true) // выключить editMode при нажатии Enter
+        onEnter // если есть пропс onEnter
+        && onEnter() // то вызвать его
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
-        // setEditMode() // выключить editMode при нажатии за пределами инпута
-
+       setEditMode(false) // выключить editMode при нажатии за пределами инпута
         onBlur && onBlur(e)
     }
     const onDoubleClickCallBack = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
-        // setEditMode() // включить editMode при двойном клике
+        setEditMode(true) // включить editMode при двойном клике
 
         onDoubleClick && onDoubleClick(e)
     }
 
-    const spanClassName = `${'сделать красивый стиль для спана'} ${className}`
+    const spanClassName = `${st.spanbox} ${className}`
 
     return (
         <>
@@ -56,18 +55,18 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
                         autoFocus // пропсу с булевым значением не обязательно указывать true
                         onBlur={onBlurCallback}
                         onEnter={onEnterCallback}
-
+                        className={st.input}
                         {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
                     />
                 ) : (
                     <span
                         onDoubleClick={onDoubleClickCallBack}
                         className={spanClassName}
-
+                        style={{cursor: 'pointer'}}
                         {...restSpanProps}
                     >
                         {/*если нет захардкодженного текста для спана, то значение инпута*/}
-                        {children || restProps.value}
+                        ✎{children || restProps.value}
                     </span>
                 )
             }
